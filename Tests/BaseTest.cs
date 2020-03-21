dde2139d5718521b2275ababe919c46bea474ac1
olszewm4 <m.olszewski1992@gmail.com>
2020-03-21 19:35:39 +0100

    Azure Pipelines Example project

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class BaseTest
+    {
+        protected readonly IFixture Fixture;
+
+        public BaseTest()
+        {
+            Fixture = new Fixture().Customize(new AutoMoqCustomization());
+        }
+    }
+}
---
+++ Tests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Web_App.Controllers;
+
+namespace Tests
+{
+    [TestClass]
+    public class HomeControllerTests : BaseTest
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            //Arrange
+            var logger = Fixture.Freeze<Mock<ILogger<HomeController>>>();
+
+            var sut = new HomeController(logger.Object);
+
+            //Act
+            var result = sut.Index();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+        }
+    }
+}
